5ed2cb1a83c3a79cba8815cfe8703966aee282f0
Seda Kunda <sedakunda@gmail.com>
2016-04-29 13:05:54 +0200

    Create WelcomeToJava.java

---
+++ Java/Introduction/WelcomeToJava.java
@@ -0,0 +1,22 @@
+/*
+Welcome to the world of Java! Just print "Hello World." and "Hello Java." in two separate lines to complete this challenge.
+
+The code stub in the editor already creates the main function and solution class. All you have to do is copy and paste the following lines inside the main function.
+
+System.out.println("Hello World.");
+System.out.println("Hello Java.");
+Sample Output
+
+Hello World.
+Hello Java.
+*/
+
+public class WelcomeToJava {
+   
+   public static void main(String []argv)
+   {
+      System.out.println("Hello World.");
+      System.out.println("Hello Java.");
+   }
+
+}
