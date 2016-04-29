63cf32881447501921b63411407f299fa8e6b0ab
Seda Kunda <sedakunda@gmail.com>
2016-04-29 13:23:24 +0200

    Using standard input and output

--- Java/Introduction/InOut.java
+++ Java/Introduction/InOut.java
@@ -1,6 +1,4 @@
 /*
-In most of the HackerRank challenges, you need to read input from stdin (standard input) and write your output in stdout (standard output).
-
 One way to take input from stdin is to use the Scanner class and read in from System.in.
 
 You can write your output to stdout by simply using the System.out.println(String) function.
@@ -8,12 +6,11 @@ You can write your output to stdout by simply using the System.out.println(Strin
 In this problem, you need to read 3 integers from stdin and print them in stdout.
 
 Sample input:
-
 42
 100
 125
-Sample output:
 
+Sample output:
 42
 100
 125
