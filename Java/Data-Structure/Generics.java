f6c65001b8b7fa2ad1b091c8d89a135ca0264e19
Seda Kunda <sedakunda@gmail.com>
2016-05-18 10:37:05 +0200

    Create Generics.java

---
+++ Java/Data-Structure/Generics.java
@@ -0,0 +1,48 @@
+/*
+Generic methods are a very efficient way to handle multiple datatypes using a single method. This problem will test your knowledge on Java Generic methods.
+
+Let's say you have an integer array and a string array. You have to write a single method printArray that can print all the elements of both arrays. The method should be able to accept both integer arrays or string arrays.
+
+You are given code in the editor. Complete the code so that it prints the following lines:
+
+1
+2
+3
+Hello
+World
+*/
+import java.io.IOException;
+import java.lang.reflect.Method;
+class Printer
+{
+   //Write your code here
+ public static < E > void printArray( E[] inputArray )
+   {
+      // Display array elements              
+         for ( E element : inputArray ){        
+            System.out.println(element);
+         }
+    }
+}
+
+public class Solution {
+
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+      
+    }
+}
