b68d12229dbf00c8a56d5ee652c076408ae28f32
Seda Kunda <sedakunda@gmail.com>
2019-02-09 18:00:12 +0000

    Update Factory.java

--- Java/Advanced/Factory.java
+++ Java/Advanced/Factory.java
@@ -2,7 +2,6 @@
 According to Wikipedia, a factory is simply an object that returns another object from some other method call, which is assumed to be "new".
 In this problem, you are given an interface Food. There are two classes Pizza and Cake which implement the Food interface, and they both contain a method getType().
 The main function in the Main class creates an instance of the FoodFactory class. The FoodFactory class contains a method getFood(String) that returns a new instance of Pizza or Cake according to its parameter.
-
 Sample Input 1
 cake
 Sample Output 1
