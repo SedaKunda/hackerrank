9c3054f87dfeb9ed3cbb263766469ea1240aced4
pratikdesai189 <pratikdesai189@gmail.com>
2022-04-18 18:09:24 +0530

    JAVA Loops I

---
+++ Java/Introduction/Loops I.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(N+ " x " + i + " = " +N*i);
+        }
+    }
+}
